1557faec1c6927851e489dc3d4d24d701b3c205e
Abhinavxox <abhinavxox@gmail.com>
2023-09-21 23:01:06 +0545

    Add : Added auctions hero

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Auctions from "../components/Auctions";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Auctions />
     </div>
   );
 };
